60863c71c13a940b4c903c111c9ece21ab1a873c
snh <snh@f6f087ea-e415-1640-bdc7-6a826d8ea1d5>
2013-11-24 22:19:54 +0000

    建立skindemo项目，创建CMake项目模板
    git-svn-id: https://10.0.0.7:8443/svn/learn-havok/trunk@2 f6f087ea-e415-1640-bdc7-6a826d8ea1d5

---
+++ include/XkConfig.h
@@ -0,0 +1,19 @@
+#ifndef XK_CONFIG_H
+#define XK_CONFIG_H
+
+//是否启用DebugString
+#define XK_ENABLE_DEBUG_STR 1
+
+//是否启用Debug Log
+#define XK_ENABLE_LOG_DEBUG 1
+
+//是否启用Warning Log
+#define XK_ENABLE_LOG_WARNING 1
+
+//是否启用Error Log
+#define XK_ENABLE_LOG_ERROR 1
+
+//是否启用Dump Log
+#define XK_ENABLE_LOG_DUMP 1
+
+#endif
---
+++ skindemo/main.cpp
@@ -0,0 +1,13 @@
+#include <Common/Base/hkBase.h>
+#include <Common/Base/System/hkBaseSystem.h>
+#include <Common/Base/System/Error/hkDefaultError.h>
+#include <Common/Base/Memory/System/Util/hkMemoryInitUtil.h>
+#include <Common/Base/Monitor/hkMonitorStream.h>
+#include <Common/Base/Memory/System/hkMemorySystem.h>
+#include <Common/Base/Memory/Allocator/Malloc/hkMallocAllocator.h>
+#include <Common/Base/Container/String/hkStringBuf.h>
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
