0da4171b28c101e803fc32b7442a825b43efd674
snh <snh@f6f087ea-e415-1640-bdc7-6a826d8ea1d5>
2013-11-25 17:16:55 +0000

    git-svn-id: https://10.0.0.7:8443/svn/learn-havok/trunk@5 f6f087ea-e415-1640-bdc7-6a826d8ea1d5

--- include/XkConfig.h
+++ include/XkConfig.h
@@ -16,4 +16,16 @@
 //是否启用Dump Log
 #define XK_ENABLE_LOG_DUMP 1
 
+//使用哪一个物理引擎
+#define XK_PHYSICS_LIB_PHYSX 0
+#define XK_PHYSICS_LIB_HAVOK 1
+
+//默认使用Havok，Linux平台下使用PhysX
+#define XK_PHYSICS_LIB XK_PHYSICS_LIB_HAVOK
+#if defined(LINUX) || defined(_LINUX)
+	#define XK_PHYSICS_LIB XK_PHYSICS_LIB_PHYSX
+#endif
+
+#define XK_HAVOK_ENABLE_VDB 0
+
 #endif
